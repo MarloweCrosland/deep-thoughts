3371d6faf14e304e5e453b213ec8af2feea1f177
marlowe <marlowepdx@gmail.com>
2022-09-25 12:47:50 -0700

    code snapshot update plus added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,32 +1,56 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
-    // find all users query
+      // get all users
     users: async () => {
       return User.find()
         .select('-__v -password')
         .populate('thoughts')
         .populate('friends');
     },
-    //parent is a placeholder we dont need here
-    // find user by username query
+    // get user by username
     user: async (parent, { username }) => {
       return User.findOne({ username })
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
-    // find all thoughts by username query
+    // get thoughts by a user
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
-    // find thought by id query
-    // destructure the id argument value to make it a local variable
+    // get though by id
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
     }
+  },
+  Mutation: {
+      //create new user 
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+
+        return user;
+      },
+
+      //login mutations
+      login: async () => {
+        const user = await User.findOne({ email });
+
+        if(!user) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if(!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        return user
+      }
   }
 };
 
