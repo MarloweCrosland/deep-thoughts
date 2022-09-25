e8afa3579db3beefccd2270cab395b7cc9a40a90
marlowe <marlowepdx@gmail.com>
2022-09-25 13:32:04 -0700

    added mutations schema

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,56 +1,111 @@
-const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+// QUERIES--------------------------------
   Query: {
-      // get all users
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
     users: async () => {
       return User.find()
         .select('-__v -password')
         .populate('thoughts')
         .populate('friends');
     },
-    // get user by username
     user: async (parent, { username }) => {
       return User.findOne({ username })
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
     },
-    // get thoughts by a user
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
-    // get though by id
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
     }
   },
+// MUTATIONS ---------------------------------------
   Mutation: {
-      //create new user 
-      addUser: async (parent, args) => {
-        const user = await User.create(args);
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-        return user;
-      },
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-      //login mutations
-      login: async () => {
-        const user = await User.findOne({ email });
-
-        if(!user) {
-            throw new AuthenticationError('Incorrect credentials');
-        }
-
-        const correctPw = await user.isCorrectPassword(password);
-
-        if(!correctPw) {
-            throw new AuthenticationError('Incorrect credentials');
-        }
-
-        return user
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
       }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // without the { new: true } flag in User.findByIdAndUpdate(), 
+    // Mongo would return the original document instead of the updated document.
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return thought;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedThought;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    }
   }
 };
 
